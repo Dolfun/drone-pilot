c67d29d21a157df20669b9927c12007af0569352
Dolfun <abhinav150104@gmail.com>
2023-12-18 09:32:41 +0530

    Change interface from singleton to free functions

--- main/WifiManager.cc
+++ main/WifiManager.cc
@@ -1,5 +1,7 @@
 #include "WifiManager.h"
 #include "freertos/FreeRTOS.h"
+#include "freertos/event_groups.h"
+#include "esp_event.h"
 #include "nvs_flash.h"
 #include "esp_netif.h"
 #include "esp_wifi.h"
@@ -7,78 +9,88 @@
 #include <algorithm>
 #include <string>
 
+#define WIFI_LOG_TAG "wifi_manager"
+
 const std::string wifi_ssid { CONFIG_ESP_WIFI_SSID     };
 const std::string wifi_pass { CONFIG_ESP_WIFI_PASSWORD };
 
-WifiManager::WifiManager() {
+void WifiManager::init() {
+  // Initialize non-volatile storage
   auto error = nvs_flash_init();
   if (error == ESP_ERR_NVS_NO_FREE_PAGES || error == ESP_ERR_NVS_NEW_VERSION_FOUND) {
     ESP_ERROR_CHECK(nvs_flash_erase());
     error = nvs_flash_init();
   }
   ESP_ERROR_CHECK(error);
-
+  
+  // Initialize netif
   ESP_ERROR_CHECK(esp_netif_init());
   ESP_ERROR_CHECK(esp_event_loop_create_default());
   esp_netif_create_default_wifi_sta();
 
+  // Configure Wi-Fi
   wifi_init_config_t wifi_init_config = WIFI_INIT_CONFIG_DEFAULT();
   ESP_ERROR_CHECK(esp_wifi_init(&wifi_init_config));
 
-  wifi_scan_threshold_t wifi_scan_threshold{};
+  wifi_scan_threshold_t wifi_scan_threshold {};
   wifi_scan_threshold.authmode = WIFI_AUTH_WPA_PSK;
 
-  wifi_sta_config_t wifi_sta_config{};
+  wifi_sta_config_t wifi_sta_config {};
   std::copy(wifi_ssid.begin(), wifi_ssid.end(), wifi_sta_config.ssid);
   std::copy(wifi_pass.begin(), wifi_pass.end(), wifi_sta_config.password);
   wifi_sta_config.threshold = wifi_scan_threshold;
 
   wifi_config_t wifi_config = {
-    .sta{wifi_sta_config}
+    .sta { wifi_sta_config }
   };
 
   ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
   ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config));
-  ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT, WIFI_EVENT_STA_START, 
-                  wifi_start_callback, nullptr, nullptr));
-  ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT, IP_EVENT_STA_GOT_IP, 
-                  ip_get_callback, static_cast<void*>(this), nullptr));
-  ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT, WIFI_EVENT_STA_DISCONNECTED, 
-                  wifi_disconnect_callback, static_cast<void*>(this), nullptr));
+
+  // Register event handles
+  esp_event_handler_instance_register(
+    WIFI_EVENT, WIFI_EVENT_STA_START, 
+    [] (void*, esp_event_base_t, int32_t, void*) {
+      esp_wifi_connect();
+    },
+    nullptr, nullptr
+  );
+
+  esp_event_handler_instance_register(
+    IP_EVENT, IP_EVENT_STA_GOT_IP, 
+    [] (void*, esp_event_base_t, int32_t, void* event_data) {
+      auto event = static_cast<ip_event_got_ip_t*>(event_data);
+      char ip_address[16];
+      sprintf(ip_address, IPSTR, IP2STR(&event->ip_info.ip));
+      ESP_LOGI(WIFI_LOG_TAG, "wifi connected!\n ip address: %s", ip_address);
+    },
+    nullptr, nullptr
+  );
+
+  esp_event_handler_instance_register(
+    WIFI_EVENT, WIFI_EVENT_STA_DISCONNECTED, 
+    [] (void*, esp_event_base_t, int32_t, void*) {
+      ESP_LOGI(WIFI_LOG_TAG, "wifi disconnected!\n retrying to connect...\n");
+    esp_wifi_connect();
+    },
+    nullptr, nullptr
+  );
 }
 
 void WifiManager::start() {
-    ESP_ERROR_CHECK(esp_wifi_start());
-  }
-
-void WifiManager::wifi_start_callback(void*, esp_event_base_t, int32_t, void*) {
-  esp_wifi_connect();
+  ESP_ERROR_CHECK(esp_wifi_start());
 }
 
-void WifiManager::ip_get_callback(void* arg, esp_event_base_t, int32_t, void* event_data) {
-  auto event = static_cast<ip_event_got_ip_t*>(event_data);
-  char ip_address[16];
-  sprintf(ip_address, IPSTR, IP2STR(&event->ip_info.ip));
-  ESP_LOGI(WIFI_LOG_TAG, "got ip: %s", ip_address);
-  
-  auto wifi_manager = static_cast<WifiManager*>(arg);
-  wifi_manager->is_connected = true;
-  wifi_manager->once_flag = true;
-  if (wifi_manager->on_connect_callback) {
-    wifi_manager->on_connect_callback();
-  }
+void WifiManager::register_on_connect_callback(void* arg, esp_event_handler_t callback) {
+  esp_event_handler_instance_register(
+    IP_EVENT, IP_EVENT_STA_GOT_IP, 
+    callback, arg, nullptr
+  );
 }
 
-void WifiManager::wifi_disconnect_callback(void* arg, esp_event_base_t, int32_t, void*) {
-  auto wifi_manager = static_cast<WifiManager*>(arg);
-  wifi_manager->is_connected = false;
-  if (wifi_manager->once_flag) {
-    wifi_manager->once_flag = false;
-    if (wifi_manager->on_disconnect_callback) {
-      wifi_manager->on_disconnect_callback();
-    }
-  }
-  
-  ESP_LOGI(WIFI_LOG_TAG, "retrying to connect");
-  esp_wifi_connect();
+void WifiManager::register_on_disconnect_callback(void* arg, esp_event_handler_t callback) {
+  esp_event_handler_instance_register(
+    WIFI_EVENT, WIFI_EVENT_STA_DISCONNECTED, 
+    callback, arg, nullptr
+  );
 }
--- main/WifiManager.h
+++ main/WifiManager.h
@@ -1,48 +1,9 @@
 #pragma once
-
-#include "freertos/FreeRTOS.h"
-#include "freertos/event_groups.h"
 #include "esp_event.h"
-#include <functional>
-
-#define WIFI_LOG_TAG "wifi_manager"
-
-using WifiManagerCallback = std::function<void()>;
-class WifiManager {
-public:
-  WifiManager(WifiManager&) = delete;
-  WifiManager& operator=(const WifiManager&) = delete;
-  WifiManager(WifiManager&&) = delete;
-  WifiManager& operator=(WifiManager&&) = delete;
-
-  static WifiManager& instance() {
-    static WifiManager wifi_manager;
-    return wifi_manager;
-  }
-
-  void register_on_connect_callback(WifiManagerCallback callback) {
-    on_connect_callback = callback;
-  }
-
-  void register_on_disconnect_callback(WifiManagerCallback callback) {
-    on_disconnect_callback = callback;
-  }
-
+namespace WifiManager {
+  void init();
   void start();
 
-  bool connected() const { 
-    return is_connected;
-  }
-private:
-  bool is_connected { false };
-  bool once_flag    { false };
-
-  WifiManager();
-
-  WifiManagerCallback on_connect_callback{};
-  WifiManagerCallback on_disconnect_callback{};
-
-  static void wifi_start_callback(void*, esp_event_base_t, int32_t, void*);
-  static void ip_get_callback(void*, esp_event_base_t, int32_t, void*);
-  static void wifi_disconnect_callback(void*, esp_event_base_t, int32_t, void*);
+  void register_on_connect_callback(void*, esp_event_handler_t);
+  void register_on_disconnect_callback(void*, esp_event_handler_t);
 };
