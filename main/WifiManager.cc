03f04c96458d3854f48620e94d64778c995975d4
Dolfun <abhinav150104@gmail.com>
2023-12-18 09:37:41 +0530

    Remove unnecessary include files

--- main/WifiManager.cc
+++ main/WifiManager.cc
@@ -1,9 +1,5 @@
 #include "WifiManager.h"
-#include "freertos/FreeRTOS.h"
-#include "freertos/event_groups.h"
-#include "esp_event.h"
 #include "nvs_flash.h"
-#include "esp_netif.h"
 #include "esp_wifi.h"
 #include "esp_log.h"
 #include <algorithm>
