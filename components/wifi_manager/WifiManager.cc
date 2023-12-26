aac294a4f05385c85ead7d58aab9ea1dbd0972c6
Dolfun <abhinav150104@gmail.com>
2023-12-27 03:19:08 +0530

    Refactor code

--- components/wifi_manager/WifiManager.cc
+++ components/wifi_manager/WifiManager.cc
@@ -26,50 +26,35 @@ void WifiManager::init() {
   wifi_init_config_t wifi_init_config = WIFI_INIT_CONFIG_DEFAULT();
   ESP_ERROR_CHECK(esp_wifi_init(&wifi_init_config));
 
-  wifi_scan_threshold_t wifi_scan_threshold {};
-  wifi_scan_threshold.authmode = WIFI_AUTH_WPA_PSK;
-
   wifi_sta_config_t wifi_sta_config {};
+  wifi_sta_config.threshold.authmode = WIFI_AUTH_WPA_PSK;
   std::memcpy(wifi_sta_config.ssid, wifi_ssid, sizeof(wifi_ssid));
   std::memcpy(wifi_sta_config.password, wifi_pass, sizeof(wifi_pass));
   
-  wifi_sta_config.threshold = wifi_scan_threshold;
-
-  wifi_config_t wifi_config = {
-    .sta { wifi_sta_config }
-  };
-
+  wifi_config_t wifi_config = { .sta { wifi_sta_config } };
   ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
   ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config));
 
   // Register event handles
-  esp_event_handler_instance_register(
-    WIFI_EVENT, WIFI_EVENT_STA_START, 
-    [] (void*, esp_event_base_t, int32_t, void*) {
-      esp_wifi_connect();
-    },
-    nullptr, nullptr
-  );
+  auto start_callback = [] (void*, esp_event_base_t, int32_t, void*) {
+    esp_wifi_connect();
+  };
 
-  esp_event_handler_instance_register(
-    IP_EVENT, IP_EVENT_STA_GOT_IP, 
-    [] (void*, esp_event_base_t, int32_t, void* event_data) {
-      auto event = static_cast<ip_event_got_ip_t*>(event_data);
-      char ip_address[16];
-      sprintf(ip_address, IPSTR, IP2STR(&event->ip_info.ip));
-      ESP_LOGI(WIFI_LOG_TAG, "wifi connected to: %s", ip_address);
-    },
-    nullptr, nullptr
-  );
+  auto connect_callback = [] (void*, esp_event_base_t, int32_t, void* event_data) {
+    auto event = static_cast<ip_event_got_ip_t*>(event_data);
+    char ip_address[16];
+    sprintf(ip_address, IPSTR, IP2STR(&event->ip_info.ip));
+    ESP_LOGI(WIFI_LOG_TAG, "wifi connected to: %s", ip_address);
+  };
 
-  esp_event_handler_instance_register(
-    WIFI_EVENT, WIFI_EVENT_STA_DISCONNECTED, 
-    [] (void*, esp_event_base_t, int32_t, void*) {
+  auto disconnect_callback = [] (void*, esp_event_base_t, int32_t, void*) {
       ESP_LOGI(WIFI_LOG_TAG, "retrying to connect...");
     esp_wifi_connect();
-    },
-    nullptr, nullptr
-  );
+    };
+
+  esp_event_handler_instance_register(WIFI_EVENT, WIFI_EVENT_STA_START, start_callback, nullptr, nullptr);
+  esp_event_handler_instance_register(IP_EVENT, IP_EVENT_STA_GOT_IP, connect_callback, nullptr, nullptr);
+  esp_event_handler_instance_register(WIFI_EVENT, WIFI_EVENT_STA_DISCONNECTED, disconnect_callback, nullptr, nullptr);
 }
 
 void WifiManager::start() {
