097c20e61e4132251c13670ef4500e161ed190c0
Dolfun <abhinav150104@gmail.com>
2024-01-14 02:09:44 +0530

    Refactor code

--- components/wifi/Wifi.cc
+++ components/wifi/Wifi.cc
@@ -9,7 +9,6 @@ char wifi_ssid[32] = CONFIG_ESP_WIFI_SSID;
 char wifi_pass[64] = CONFIG_ESP_WIFI_PASSWORD;
 
 void Wifi::init() {
-  // Initialize non-volatile storage
   auto error = nvs_flash_init();
   if (error == ESP_ERR_NVS_NO_FREE_PAGES || error == ESP_ERR_NVS_NEW_VERSION_FOUND) {
     ESP_ERROR_CHECK(nvs_flash_erase());
@@ -17,12 +16,10 @@ void Wifi::init() {
   }
   ESP_ERROR_CHECK(error);
   
-  // Initialize netif
   ESP_ERROR_CHECK(esp_netif_init());
   ESP_ERROR_CHECK(esp_event_loop_create_default());
   esp_netif_create_default_wifi_sta();
 
-  // Configure Wi-Fi
   wifi_init_config_t wifi_init_config = WIFI_INIT_CONFIG_DEFAULT();
   ESP_ERROR_CHECK(esp_wifi_init(&wifi_init_config));
 
@@ -35,7 +32,6 @@ void Wifi::init() {
   ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
   ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config));
 
-  // Register event handles
   auto start_callback = [] (void*, esp_event_base_t, int32_t, void*) {
     esp_wifi_connect();
   };
@@ -48,9 +44,9 @@ void Wifi::init() {
   };
 
   auto disconnect_callback = [] (void*, esp_event_base_t, int32_t, void*) {
-      ESP_LOGI(WIFI_LOG_TAG, "retrying to connect...");
-      esp_wifi_connect();
-    };
+    ESP_LOGI(WIFI_LOG_TAG, "retrying to connect...");
+    esp_wifi_connect();
+  };
 
   esp_event_handler_instance_register(WIFI_EVENT, WIFI_EVENT_STA_START, start_callback, nullptr, nullptr);
   esp_event_handler_instance_register(IP_EVENT, IP_EVENT_STA_GOT_IP, connect_callback, nullptr, nullptr);
