8a84d9dfe75eb3b8ba57af25ece40e5ee8259b22
Dolfun <abhinav150104@gmail.com>
2024-03-17 03:34:43 +0530

    Remove asio

--- components/application/Application.cc
+++ components/application/Application.cc
@@ -1,11 +1,5 @@
 #include "Application.h"
-#include "ConnectionTask.h"
 #include "Wifi.h"
-
-static ConnectionTask connection_task;
-
-Application::Application() 
-  : client(connection_task.get_io_context()) {}
 
 void Application::run() {
   Wifi::start();
--- components/application/Application.h
+++ components/application/Application.h
@@ -1,12 +1,8 @@
 #pragma once
-#include "Client.h"
 
 class Application {
 public:
-  Application();
-
   void run();
   
 private:
-  Client client;
 };
